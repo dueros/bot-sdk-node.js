cac0bc006a71302a067f7ad3ac4a725273c42d16
yuanpeng <yuanpeng01@baidu.com>
2017-11-16 15:48:56 +0800

    OPENDUER-454
    Change-Id: I9b1e64081291f85e039b606da62775c8b1f802a1

--- samples/personal-income-tax/index.js
+++ samples/personal-income-tax/index.js
@@ -27,9 +27,30 @@ app.post('/', (req, res) => {
         // 开启签名认证
         b.initCertificate(req.headers, req.rawBody).enableVerifyRequestSign();
 
-        b.run().then(function(result){
-            res.send(result);
+        
+        /**
+         * 需要监控功能
+         * 
+         * bot-sdk 集成了监控sdk，参考：https://www.npmjs.com/package/bot-monitor-sdk
+         * 打开此功能，对服务的性能有一定的耗时增加。另外，需要在DBP平台上面上传public key，这里使用私钥签名
+         * 文档参考：https://dueros.baidu.com/didp/doc/dueros-bot-platform/dbp-deploy/authentication_markdown
+         */
+        b.setPrivateKey(__dirname + '/rsa_private_key.pem').then(function(key){
+            // 0: debug  1: online
+            b.botMonitor.setEnvironmentInfo(key, 0);
+
+            b.run().then(function(result){
+                res.send(result);
+            });
+        }, function(err){
+            console.error('error'); 
         });
+
+        
+        // 不需要监控
+        //b.run().then(function(result){
+        //    res.send(result);
+        //});
     });
 }).listen(8014);
 
