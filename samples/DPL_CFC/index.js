24924e0a928949b23af6c32ba2dc6f7ba302c0ad
yelvye <yelvye@baidu.com>
2019-06-24 11:25:13 +0800

    DuerOS-Cloud-1780

---
+++ samples/DPL_CFC/index.js
@@ -0,0 +1,16 @@
+const Bot = require('./Bot');
+const privateKey = require("./rsaKeys.js").privateKey;
+
+
+exports.handler = function(event, context, callback) {
+    try {
+        let b = new Bot(event);
+        // 0: debug  1: online
+        b.botMonitor.setEnvironmentInfo(privateKey, 0);
+        b.run().then(function(result) {
+            callback(null, result);
+        }).catch(callback);
+    } catch (e) {
+        callback(e);
+    }
+}
